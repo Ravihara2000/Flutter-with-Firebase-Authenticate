473d9ef2d86a9b70b3f6121396eaa1865c0d4535
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-10-01 18:15:25 +0530

    create the stream for checking auth changes

--- projX/lib/services/auth.dart
+++ projX/lib/services/auth.dart
@@ -1,8 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:projx/models/UserModel.dart';
 
 class AuthServices {
   //firebase instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//create a user fromfirebase user with id
+  UserModel? _userWithFirebaseUserUid(User? user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
+
+  //create the stream for checking auth changes in the user
+  Stream<UserModel?> get user {
+    return _auth.authStateChanges().map(_userWithFirebaseUserUid);
+  }
+
+
 
   //signin anonymous
   Future signInAnonymously() async {
