2d1438892b9919138c33e4556257e7362af51c6b
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-10-01 20:41:32 +0530

    signout method modified

--- projX/lib/screens/home/home.dart
+++ projX/lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projx/services/auth.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,15 +9,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  //create a object from auth Services
+  final AuthServices _auth = AuthServices();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home:Scaffold(
         appBar: AppBar(
+          
           title: Text("Home"),
+          actions: [
+            ElevatedButton(onPressed: () async{
+              await _auth.signOut();
+            },
+             child: const Icon(Icons.logout),
+             )
+          ],
         )
       )
-    )
     );
+
   }
 }
