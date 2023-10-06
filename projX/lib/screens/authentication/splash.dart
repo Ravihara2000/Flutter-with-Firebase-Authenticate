0eff2563067151f8c47be46fdf3be76494e416e6
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-10-06 19:49:16 +0530

    splash page done

--- projX/lib/screens/authentication/splash.dart
+++ projX/lib/screens/authentication/splash.dart
@@ -3,10 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:projx/constants/colors.dart';
 import 'package:projx/screens/wrapper.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({super.key});
 
   @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(
+      Duration(milliseconds: 4000),
+      () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Wrapper()));
+      },
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgBlack,
@@ -27,17 +47,23 @@ class Splash extends StatelessWidget {
               SizedBox(
                 height: 110,
               ),
-              Text("| All rights reserved |",style: TextStyle(
-                color: mainYellow,
-              ),),
+              Text(
+                "| All rights reserved |",
+                style: TextStyle(
+                  color: mainYellow,
+                ),
+              ),
               SizedBox(
                 height: 10,
               ),
-              Text("Application By Ravihara De Silva",style:TextStyle(
-                fontWeight: FontWeight.w800,
-                color: mainYellow,
-                fontSize: 20,
-              ),),
+              Text(
+                "Application By Ravihara De Silva",
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: mainYellow,
+                  fontSize: 20,
+                ),
+              ),
             ],
           ),
         ),
